de2730609164b8d868b22a961303edb86961c12a
joão.frança <joaofrancaf@hotmail.com>
2022-09-21 16:13:38 -0300

    second

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import { globaStyles } from "../styles/global"
 import logoimg from '../assests/logo.svg'
 import { Container, Header } from "../styles/pages/app";
 
+import Image from 'next/future/image'
+
 globaStyles();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoimg.src} alt=""/>
+        <Image src={logoimg} alt=""/>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,84 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
+import Image from "next/future/image";
 
+import {useKeenSlider } from 'keen-slider/react'
+
+import camiseta1 from '../assests/camisetas/1.png'
+import camiseta2 from '../assests/camisetas/2.png'
+import camiseta3 from '../assests/camisetas/3.png'
+
+import 'keen-slider/keen-slider.min.css';
+
+//import { styled } from "../styles"
+
+/*
 const Button = styled('button',{
   backgroundColor: '$green300',/*css aqui no stitches é um objeto javascript*/
+  /*
   borderRadius: 4,
   border: 0,
   padding:'4p 8px',
 
   /*encadeamento de estilização aqui*/
+  /*
   span: {
     fontWeight: 'bold'
   },
 
   '&:hover': { /*o e comercial (&) indica o proprio elemento (o buttom)*/
+  /*
     filter: 'brightness(0.8)'
   },
   
-})
+})  
+*/
+
+
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing:48,
+    }
+  })
   return (
-    <Button>
-      <span>teste</span> enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>RS 79,99</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>RS 79,99</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>RS 79,99</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>RS 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
